be1f807f7ee55a4ca6361de0f9e636d0b258a74d
Jack Huey <jhhuey@icloud.com>
2026-07-03 10:38:26 -0500

    Add docstrings

--- blobfish.py
+++ blobfish.py
@@ -82,11 +82,30 @@ class Binary:
         return len(self.__data) // (8 if (byteIndexes if self.byteIndexes is None else self.byteIndexes) else 1)
 
     def write(self, file: "io.BufferedRandom | io.BufferedWriter") -> int:
+        """
+        Writes the binary data to a file.
+
+        Parameters:
+        file (``io.BufferedRandom`` or ``io.BufferedWriter``): The file to write to.
+
+        Returns:
+        ``int``: The number of bytes written to the file.
+        """
         if not isinstance(file, (io.BufferedRandom, io.BufferedWriter)) or "b" not in file.mode or not file.writable():
             raise ValueError("File must be opened in binary write mode (e.g. \"wb\" or \"rb+\")")
         return file.write(bytes(self.encode(int)))
 
     def encode(self, encodingType: type[str] | type[int] = str) -> str | list[int]:
+        """
+        Encodes the binary data into a string or list of ints.
+
+        Parameters:
+        encodingType (``str`` or ``int``, optional): The type to encode the binary data as. Default is ``str``.
+
+        Returns:
+        Passing ``str`` will return a string of the encoded binary data.
+        Passing ``int`` will return a list of the ascii codes as integers.
+        """
         if encodingType == str:
             return "".join(chr(i) for i in self.encode(int))
         elif encodingType == int:
@@ -97,12 +116,40 @@ class Binary:
             raise TypeError("Encoding type must be str or int")
 
 def fromBytes(bytes: bytes) -> "Binary":
+    """
+    Creates a `blobfish.Binary` instance from a bytes object.
+
+    Parameters:
+    bytes (``bytes``): The input bytes object to convert.
+
+    Returns:
+    `blobfish.Binary`: An instance representing the binary data of the input bytes object.
+    """
     return Binary("".join(f"{byte:08b}" for byte in bytes))
 
 def fromString(string: str, encoding: str="utf-8") -> "Binary":
+    """
+    Creates a `blobfish.Binary` instance from a string.
+
+    Parameters:
+    string (``str``): The input string to convert.
+    encoding (``str``, optional): The encoding to use for the string. Default is ``"utf-8"``.
+
+    Returns:
+    `blobfish.Binary`: An instance representing the binary data of the input string.
+    """
     return fromBytes(string.encode(encoding))
 
 def fromFile(file: "io.BufferedRandom | io.BufferedReader") -> "Binary":
+    """
+    Creates a `blobfish.Binary` instance from the contents of a binary file.
+
+    Parameters:
+    file (``io.BufferedRandom`` or ``io.BufferedReader``): The file to read from.
+
+    Returns:
+    `blobfish.Binary`: An instance representing the binary data of the input file.
+    """
     if not isinstance(file, (io.BufferedRandom, io.BufferedReader)) or "b" not in file.mode or not file.readable():
             raise ValueError("File must be opened in binary read mode (e.g. \"rb\" or \"wb+\")")
     return fromBytes(file.read())
